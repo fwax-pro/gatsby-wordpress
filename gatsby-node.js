df6848e313e0042c26d0134ff38902f65e4839fb
fwax <fwax.pro@gmail.com>
2021-02-17 19:42:03 +0100

    creation project

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,46 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require(`path`)
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ actions, graphql, reporter }) => {
+    const { createPage } = actions;
+    const result = await graphql(`
+        {
+            allWpPost(sort: {fields: [date]}) {
+                edges {
+                    node {
+                        title
+                        excerpt
+                        slug
+                        date(formatString: "MM-DD-YYYY")
+                        author {
+                            node {
+                                name
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `);
+
+    if (result.errors) {
+        reporter.panicOnBuild(`Error while running GraphQL query.`);
+        return
+    }
+
+    const posts = result.data.allWpPost.edges;
+
+    posts.forEach(({ node }) => {
+        createPage({
+          // Decide URL structure
+          path: node.slug,
+          // path to template
+          component: path.resolve(`./src/templates/blog-post.js`),
+          context: {
+            // This is the $slug variable
+            // passed to blog-post.js
+            slug: node.slug,
+          },
+        })
+    });
+
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,40 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = ({data}) => {
+  console.log('data', data);
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h4>Posts</h4>
+      {data.allWpPost.edges.map(({ node }) => (
+        <div key={node.slug}>
+          <Link to={node.slug}>
+            <h5>{node.title}</h5>
+          </Link>
+          <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+        </div>
+      ))}
+    </Layout>
+  )
+  
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    allWpPost(sort: { fields: [date] }) {
+      edges {
+        node {
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+  }
+`
